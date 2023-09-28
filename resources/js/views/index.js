c5876edd31390457a4e10eee7c811b5fa08defb3
Zainnoeryadie Akmal sobandiar <zainnoeryadie@gmail.com>
2023-09-29 04:58:25 +0700

    fix maintenance file

--- resources/js/views/index.js
+++ resources/js/views/index.js
@@ -1,7 +1,7 @@
 // import Login from './Login.vue'
 // import Dashboard from '@/views/admin/Dashboard.vue'
 import { SurveySekolah, CreateSurveySekolah, DapilCategory, ShowSurveySekolah, Dashboard } from './admin'
-const Maintenance = () => import('./Maintenance')
+const Maintenance = () => import('./Maintenance.vue')
 const Login = () => import('./Login.vue')
 const Profile = () => import('./admin/Profile/Profile.vue')
 // const Dashboard = () => import('./admin')
